adac421da09150f0f9babb40a4e2785ec3c53be0
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-05 14:50:01 +0200

    Added the script not import

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
 
-if len(sys.argv) == 1:
-    print("0 arguments.", end="\n")
-else:
-    if len(sys.argv) == 2:
-        print("1 argument:", end="\n")
-        print("1: {}".format(sys.argv[1]), end="\n")
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0 arguments.", end="\n")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1), end="\n")
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {}".format(i, sys.argv[i]), end="\n")
+        if len(sys.argv) == 2:
+            print("1 argument:", end="\n")
+            print("1: {}".format(sys.argv[1]), end="\n")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1), end="\n")
+            for i in range(1, len(sys.argv)):
+                print("{:d}: {}".format(i, sys.argv[i]), end="\n")
