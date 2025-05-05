682b6b01fa7e38f832c420129df42f5db4a8c733
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-05 16:33:52 +0200

    Correcetd the output format

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -14,13 +14,16 @@ if __name__ == "__main__":
 
     if op == chr(43):
         result = add(a, b)
+        print("{:d} + {:d} = {:d}".format(a, b, result), end="\n")
     elif op == chr(45):
         result = sub(a, b)
+        print("{:d} - {:d} = {:d}".format(a, b, result), end="\n")
     elif op == chr(42):
         result = mul(a, b)
+        print("{:d} * {:d} = {:d}".format(a, b, result), end="\n")
     elif op == chr(47):
         result = div(a, b)
+        print("{:d} / {:d} = {:d}".format(a, b, result), end="\n")
     else:
         print("Unknown operator. Available operators: +, -, * and /", end="\n")
         exit(1)
-    print("{:d}".format(result), end="\n")
