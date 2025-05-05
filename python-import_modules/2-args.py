012d655baf684b6e3f59b4d0430b0c7b55f52706
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-05 14:48:59 +0200

    Solution for task 2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) == 1:
+    print("0 arguments.", end="\n")
+else:
+    if len(sys.argv) == 2:
+        print("1 argument:", end="\n")
+        print("1: {}".format(sys.argv[1]), end="\n")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1), end="\n")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]), end="\n")
