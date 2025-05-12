14b85bfa903fd444154c02f8e985b64af7c29f3d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-12 17:52:23 +0200

    Pycodestyled it

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
 Module to print an indented text.
-This module provides a single function that prints a text with 2 new lines after
-each of these characters: . ? and :
+This module provides a single function that prints
+a text with 2 new lines after each of these characters:
+. ? and :
 """
 
 
