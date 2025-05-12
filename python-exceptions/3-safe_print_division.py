6e6e43029f832cf1f24a03facd0281769c71020c
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-12 13:55:31 +0200

    Solution for task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        if res is None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {:f}".format(res))
+        return res
