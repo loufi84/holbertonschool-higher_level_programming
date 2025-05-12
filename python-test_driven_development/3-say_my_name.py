9519fb9dbe5cb7616e29c84269ea3bef6ce54214
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-12 16:05:26 +0200

    Solution for task 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module to print a name.
+This module provides a single function that print a name in stdout.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Print a name in stdout.
+    Args:
+        first_name: The first name of the person
+        last_name: The last name of the person
+    Raises:
+        TypeError: If first_name or last_name aren't strings
+    """
+    # Check if first_name is a string
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    # Check if last_name is a string
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
