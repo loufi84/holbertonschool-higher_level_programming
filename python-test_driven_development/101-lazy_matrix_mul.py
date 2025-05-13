fd95b4d082aeb07e333b6024905117a2089af3d7
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-13 15:55:15 +0200

    Added a space in the hard code

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -32,10 +32,10 @@ def lazy_matrix_mul(m_a, m_b):
     # Check for empty lists
     if not m_a or m_a == [[]]:
         raise ValueError("shapes (1,0) and (2,2) not aligned: 0"
-                         "(dim 1) != 2 (dim 0)")
+                         "(dim 1) != 2 (dim 0) ")
     if not m_b or m_b == [[]]:
         raise ValueError("shapes (1,0) and (2,2) not aligned: 0"
-                         "(dim 1) != 2 (dim 0)")
+                         "(dim 1) != 2 (dim 0) ")
 
     try:
         # Numpy conversion
