1e9ee1e0d943e087564718cb86ed25ed1c0fef31
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-19 09:24:09 +0200

    Setting up the project Classes and objects

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This is a blueprint of a class that defines a square.
+
+Args:
+    None
+
+Attributes:
+            None
+'''
+
+
+class Square:
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+This is the directory for the first OOP project in Python
