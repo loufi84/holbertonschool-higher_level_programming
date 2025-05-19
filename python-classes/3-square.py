effad979bfecd1e8f2e9d0fb13fa97e268f7efae
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-19 10:06:17 +0200

    Corrected the base value of size

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
     Attribute:
                 size (int): The size of the square, must be positive
     '''
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
