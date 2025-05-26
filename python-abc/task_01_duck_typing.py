15e87142045a1438f56d73450ec9b41c5ddc8441
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-26 13:18:01 +0200

    Correction try

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -19,7 +19,7 @@ class Shape(ABC):
 class Circle(Shape):
     """Circle shape with radius."""
     def __init__(self, radius):
-        self.__radius = radius
+        self.__radius = abs(radius)
 
     def area(self):
         return  math.pi * (self.__radius ** 2)
