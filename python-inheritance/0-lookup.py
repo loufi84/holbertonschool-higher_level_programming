dee87949c79567d7193a33b68cb879c09a1ab3e3
Quentin <48597772+loufi84@users.noreply.github.com>
2025-05-26 09:24:25 +0200

    Update

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+'''
+Module to print attributes and methods of objects
+'''
 def lookup(obj):
+    '''
+    I think there is not much to say. Use dir to return all methods
+    and attributes.
+    Args:
+        obj: The object
+    '''
     return dir(obj)
