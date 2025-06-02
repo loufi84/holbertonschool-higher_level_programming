3a020a4dec9d19f3ac29f2b6148e184725b17eeb
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 15:01:05 +0200

    New test

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -18,33 +18,37 @@ def print_stats(total_size, status_counts):
         print("{}: {}".format(code, status_counts[code]))
 
 
-total_size = 0
-status_count = {}
-valid_stat_code = ['200', '301', '400', '401', '403', '404', '405', '500']
-line_count = 0
+def main():
+    total_size = 0
+    status_count = {}
+    valid_stat_code = ['200', '301', '400', '401', '403', '404', '405', '500']
+    line_count = 0
 
-try:
-    for line in sys.stdin:
-        parts = line.strip().split()
+    try:
+        for line in sys.stdin:
+            parts = line.strip().split()
 
-        if len(parts) >= 7:
-            status_code = parts[-2]
-            file_size = parts[-1]
+            if len(parts) >= 7:
+                status_code = parts[-2]
+                file_size = parts[-1]
 
-            try:
-                total_size += int(file_size)
-            except ValueError:
-                pass
+                try:
+                    total_size += int(file_size)
+                except ValueError:
+                    pass
 
-            if status_code in valid_stat_code:
-                status_count[status_code] = status_count.get(status_code, 0)+1
+                if status_code in valid_stat_code:
+                    status_count[status_code] = status_count.get(status_code, 0)+1
 
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats(total_size, status_count)
+            line_count += 1
+            if line_count % 10 == 0:
+                print_stats(total_size, status_count)
 
-except KeyboardInterrupt:
-    pass
+    except KeyboardInterrupt:
+        pass
 
-finally:
-    print_stats(total_size, status_count)
+    finally:
+        print_stats(total_size, status_count)
+
+if __name__ == "__main__":
+    main()
