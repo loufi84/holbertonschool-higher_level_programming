c501b4745be586ed96ea0c837ed278df7e52cfb1
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 11:26:44 +0200

    Solution proposition for task 9

---
+++ python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+This module provides a class that defines a student
+'''
+
+
+class Student:
+    '''
+    This class defines a student.
+    Args:
+        first_name: student's first name
+        last_name: student's last_name
+        age: student's age
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        '''
+        The function to return the dictionary description of Student
+        '''
+        return Student.__dict__
