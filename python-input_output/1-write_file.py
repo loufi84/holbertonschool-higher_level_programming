0d2df98a4a6ed29f9fb2631e945a418db0197a67
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 09:52:50 +0200

    One try

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This module provides a single function that writes a string to a text file.
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    This function writes a string to a text file, if it doesn't exist create it
+    Args:
+        filename: The file to write to
+        text: The string to write
+    '''
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
