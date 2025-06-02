b23c3d12dfd03f13cb8b1cbd88085bfb0b5f9017
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 11:40:50 +0200

    Solution for task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+This module provides a class that defines a student
+'''
+
+
+class Student:
+    '''
+    This class defines a student.
+    Args:
+        first_name: student's first name
+        last_name: student's last_name
+        age: student's age
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''
+        The function to return the dictionary description of Student
+        '''
+        if isinstance(attrs, list):
+            for element in attrs:
+                if not isinstance(element, str):
+                    return self.__dict__
+            filtered = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    filtered[key] = self.__dict__[key]
+            return filtered
+        return self.__dict__
