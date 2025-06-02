9c23fcadedb73cc2ab50b732d2549e3771663315
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 10:46:44 +0200

    This is another test with JSON

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This module provides a function that creates an object from a JSON
+'''
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    This function open a file and creates an object from the JSON string
+    Args:
+        filename: The file to check
+    '''
+    with open(filename, encoding="utf-8") as f:
+        return json.loads(f)
