cc9d812e3385db5f49cf2e6c336628fb3955d7ef
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 10:47:59 +0200

    One s can change the world

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -12,4 +12,4 @@ def load_from_json_file(filename):
         filename: The file to check
     '''
     with open(filename, encoding="utf-8") as f:
-        return json.loads(f)
+        return json.load(f)
