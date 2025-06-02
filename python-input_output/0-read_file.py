cdc4837fe20f2febe200b9b279235f1d7a4aa8b6
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 09:23:08 +0200

    Documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+'''
+This module provides a single function reading a file with "with"
+'''
+
+
 def read_file(filename=""):
+    '''
+    This is the function reading file using "with"
+    Args:
+        filename: The file to open
+    '''
     with open(filename, encoding="utf-8") as f:
         print(f.read())
