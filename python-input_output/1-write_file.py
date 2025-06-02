6d8c7e302958d82567757a89db90309ad8d91aea
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 09:54:57 +0200

    J'avais pas lu

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -12,4 +12,4 @@ def write_file(filename="", text=""):
         text: The string to write
     '''
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)
