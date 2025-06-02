aab24d512be97ee5e695ed6ce135cfdc7244d378
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-02 10:04:32 +0200

    Another test, another day

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module provides a function that serialize an object into JSON
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    This function return a serialized object in JSON
+    Args:
+        my_onj: The object to serialize
+    '''
+    return json.dumps(my_obj)
