f02af66256d2e3f43b1c384772dfadba32e26681
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-09 10:09:44 +0200

    Setting up the project and solution proposition for task 02

---
+++ restful-api/task_02_resquests.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+This module provides 2 functions to fetch datas with requests.
+'''
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    '''
+    This function will fetch posts from JSONPlaceholder and displays them
+    to the stdout.
+    '''
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print("Status code: {}".format(r.status_code))
+    if r.status_code == 200:
+        data = r.json()
+        for post in data:
+            print(post['title'])
+
+
+def fetch_and_save_posts():
+    '''
+    This function will fetch posts from JSONPlaceholder and saves them
+    to a CSV file named 'posts.csv'.
+    '''
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print("Status code: {}".format(r.status_code))
+    if r.status_code == 200:
+        data = r.json()
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as f:
+            fields = ['id', 'title', 'body']
+            writer = csv.DictWriter(f, fieldnames=fields)
+            writer.writeheader()
+
+            for row in data:
+                writer.writerow(row)
