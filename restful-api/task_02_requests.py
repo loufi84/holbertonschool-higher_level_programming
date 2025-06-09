20900051baff0f1058c9a4cd6d6b3b806f40770e
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-09 10:18:38 +0200

    Added a filtered row to the CSV writer

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -29,9 +29,10 @@ def fetch_and_save_posts():
     if r.status_code == 200:
         data = r.json()
         with open('posts.csv', 'w', newline='', encoding='utf-8') as f:
-            fields = ['id', 'title', 'body', 'userId']
+            fields = ['id', 'title', 'body']
             writer = csv.DictWriter(f, fieldnames=fields)
             writer.writeheader()
 
             for row in data:
-                writer.writerow(row)
+                filtered_row = {key: row[key] for key in fields}
+                writer.writerow(filtered_row)
