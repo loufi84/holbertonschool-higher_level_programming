1674cc6675944f8c2ba0d9b45d9b437b5a348179
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 17:47:44 +0200

    Format?

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     states = cursor.fetchall()
 
     for state in states:
-        print("{}, '{}'".format(state[0], state[1]))
+        print("({}, '{}')".format(state[0], state[1]))
 
     cursor.close()
     db.close()
