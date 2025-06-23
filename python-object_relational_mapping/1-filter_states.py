f2796c7e0f8de8acc6fbee8b69ae60c4f397a6ec
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 13:04:48 +0200

    Typos. Typos everywhere

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,15 +9,15 @@ import sys
 if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
-    db_name = sys.atgv[3]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, pwd=passwd, database=db_name)
+                         user=user, passwd=passwd, db=db_name)
 
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name"
-                   "LIKE 'N%' ORDER BY is ASC;")
+                   "LIKE 'N%' ORDER BY id ASC;")
     rows = cursor.fetchall()
 
     for id, name in rows:
