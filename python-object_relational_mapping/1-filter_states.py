359cf92f3407fc4b391175fe1d9172a9406eccce
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 13:19:14 +0200

    And now ??

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,21 +8,27 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: <username> <password> <database>")
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
         sys.exit(1)
 
-
-    user = sys.argv[1]
+    username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, passwd=password, db=db_name)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
 
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name "
-                   "LIKE BINARY 'N%' ORDER BY id ASC;")
+    toolng = "SELECT * FROM states WHERE NAME LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(toolng)
+
     rows = cursor.fetchall()
 
     for row in rows:
