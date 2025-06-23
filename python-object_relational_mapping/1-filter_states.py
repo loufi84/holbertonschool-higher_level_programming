cd2b44b60055f1420c996a77b1baf3f36837d33d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 13:07:03 +0200

    Tried something else

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
                    "LIKE 'N%' ORDER BY id ASC;")
     rows = cursor.fetchall()
 
-    for id, name in rows:
-        print("({}, '{}')".format(id, name))
+    for row in rows:
+        print(row)
 
     cursor.close()
     db.close()
