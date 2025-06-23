bb44a52d06980325fd68fd6c6d3efc39cb82d3cf
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 13:02:39 +0200

    Task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.atgv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, pwd=passwd, database=db_name)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name"
+                   "LIKE 'N%' ORDER BY is ASC;")
+    rows = cursor.fetchall()
+
+    for id, name in rows:
+        print("({}, '{}')".format(id, name))
+
+    cursor.close()
+    db.close()
