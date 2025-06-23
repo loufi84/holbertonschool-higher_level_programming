4318a4afb0dc28f2d1c04d989e1888a99f5d261f
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 17:39:03 +0200

    Task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    searched_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cursor.execute(query, (searched_name, ))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print("({}, '{}')".format(state[0], state[1]))
+
+    cursor.close()
+    db.close()
