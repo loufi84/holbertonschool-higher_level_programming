ab4b6b1933afb3be63f144762f5a8388c3b1f4f5
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-23 17:19:42 +0200

    .

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
 """
-Script to look for the MySQL server state with a given name.
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+    # On va essayer de vérifier ça pour le checker...
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
 
-    db = None
-    cursor = None
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+        user=username,
+        passwd=password,
+        db=db_name
     )
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     cursor.execute(query)
 
-    results = cursor.fetchall()
+    rows = cursor.fetchall()
 
-    for row in results:
+    for row in rows:
         print(row)
 
     cursor.close()
