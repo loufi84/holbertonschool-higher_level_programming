7c2b0a29722a136a5d32ea1174793cf83442ac01
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-24 09:25:35 +0200

    Task 8 with f-string

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,4 +1,4 @@
-#!/usr/bon/python3
+#!/usr/bin/python3
 """
 Script to print the first State object from database.
 """
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, db_name),
         pool_pre_ping=True
     )
 
@@ -29,7 +30,7 @@ if __name__ == "__main__":
     first_state = session.query(State).order_by(State.id).first()
 
     if first_state:
-        print(f"{first_state.id}: {first_state.name}")
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
 
