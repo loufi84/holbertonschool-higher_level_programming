c128a0016922d78fa3cb11075910b8dbb9e0bea2
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-24 09:05:33 +0200

    Task 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script to list all states by id using SQLAlchemy.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    session.close()
