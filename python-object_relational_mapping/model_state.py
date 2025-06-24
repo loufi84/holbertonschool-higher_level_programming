08cf5d7fd74d5993865f01bc708f4b9d9d2574ef
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-24 08:34:53 +0200

    Docstring

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    The state class that inherits from Base.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
