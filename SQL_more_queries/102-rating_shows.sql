fd2a38caff519ca6e7f5d78049c89046795e22a6
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 10:14:12 +0200

    Task 102

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title, IFNULL(SUM(tv_show_ratings.rating), 0) AS rating
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.id
+GROUP BY tv_shows.title
 ORDER BY rating DESC;
