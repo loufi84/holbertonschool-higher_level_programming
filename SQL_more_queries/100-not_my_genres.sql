d42c7e3aab4bb8f8b1d78daeda358e0764c9a82f
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 09:27:20 +0200

    Task 100 1st try

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- This script will display all genres not linked to the show Dexter
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT tv_genres_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_shows.id = tv_show_genres.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
