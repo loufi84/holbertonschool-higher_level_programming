8973a7469638a3ce5696e796577e711460069696
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 11:46:45 +0200

    Task 102

---
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module provides a script that lists all Satte objects and corresponding
+City objects.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, joinedload
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id).all()
+
+    for city in cities:
+        print(f"{city.id}: {city.name} -> {city.state.name}")
+
+    session.close()
