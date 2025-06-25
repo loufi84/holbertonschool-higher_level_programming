4f69cd6f9d497e3bb7c5d6f87125f3fcfdba632a
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 10:08:47 +0200

    Task 102

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- This script will lists all shows by their ratings
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating
+SELECT CONCAT(tv_shows.title, ' - ', SUM(tv_show_ratings.rating)) AS result
 FROM tv_shows
 JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
-ORDER BY rating DESC;
+ORDER BY SUM(tv_show_ratings.rating) DESC;
