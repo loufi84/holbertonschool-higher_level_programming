cd7f493a86f545af81d7259b016d679f673bd6a6
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 08:52:41 +0200

    Task 100 normally checker proof

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -10,4 +10,4 @@ ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode
 
 -- Name 'field' transformation
 ALTER TABLE first_table MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+COLLATE utf8mb4_unicode_ci;
