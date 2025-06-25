0b0003ddbd7f3b7c87ebe7db0e8134a3ae0ab274
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 08:59:55 +0200

    Simpler task 100 ?

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,6 +7,3 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Table transformation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Name 'field' transformation
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4;
