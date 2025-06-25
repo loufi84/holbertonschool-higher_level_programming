6e2aef6f794c90ff5136bad5aaabb7deeca3028d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 10:19:02 +0200

    Task 102

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- This script will lists all shows by their ratings
-SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rating), 0) AS rating
+SELECT tv_shows.title, COALESCE(SUM(tv_show_ratings.rate), 0) AS rating
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.title
+GROUP BY tv_shows.id
 ORDER BY rating DESC;
