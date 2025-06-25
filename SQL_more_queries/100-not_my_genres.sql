f6461cf9efa405a171a4af048cfcd7ab9a301d29
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 09:31:50 +0200

    Task 100 3rd try

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,7 +2,7 @@
 SELECT name
 FROM tv_genres
 WHERE id NOT IN (
-    SELECT tv_genres_id
+    SELECT tv_genres
     FROM tv_show_genres
     JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
     WHERE tv_shows.title = 'Dexter'
