86269ff06c91ac484ccf3561035c3f1e848ba102
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 08:47:20 +0200

    Task 100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,7 @@
 -- Transform a DB and table to UTF-8 format
+-- Select the correct DataBase
+USE hbtn_0c_0;
+
 -- DataBase transformation
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
