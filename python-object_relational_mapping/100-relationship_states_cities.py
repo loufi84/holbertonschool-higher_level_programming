204bfca24b39d066b7c8d5bae3f96a4612cae43d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-06-25 10:45:45 +0200

    Task 100

--- python-object_relational_mapping/100-relationship_states_cities.py
+++ python-object_relational_mapping/100-relationship_states_cities.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         pool_pre_ping=True
     )
 
-    Base.metadata.creat_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -18,4 +18,5 @@ class State(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     cities = relationship("City",
-                          back_populates="state", cascade="all, delete")
+                          back_populates="state", cascade="all, delete",
+                          order_by="City.id")
