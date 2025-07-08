ac3d469c7950f8190ad40e0e79f7f45f6d871576
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-08 11:44:12 +0200

    Task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 
-argv.forEach((val, index) => {
-  if (index === 0) {
-    console.log('No argument');
-  } else if (index === 1) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
-});
+if (argv === 0) {
+  console.log('No argument');
+} else if (argv === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
