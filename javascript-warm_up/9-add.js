bc1346c2b7ecdedd8b6956db2ae456ea01bd6d1d
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-08 12:44:41 +0200

    Task 9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -2,7 +2,9 @@
 const { argv } = require('node:process');
 
 function add (a, b) {
-  a = parseInt(argv[2]);
-  b = parseInt(argv[3]);
-  console.log(a + b);
+  return a + b;
 }
+
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+console.log(add(a, b));
