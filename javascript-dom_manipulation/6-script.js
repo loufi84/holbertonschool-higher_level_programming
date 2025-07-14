897f806303ce85457abcfc4ecd6478d21a66cc87
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-14 11:50:58 +0200

    Semistandarded

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
     // Transform the response in json
     .then(response => response.json())
     .then(data => {
-        // Add name datas from json objects fetched
-        const char = document.getElementById('character');
-        char.textContent = data.name;
+      // Add name datas from json objects fetched
+      const char = document.getElementById('character');
+      char.textContent = data.name;
     })
     // Try error recuperation for the first time
     .catch(error => {
-        console.error('Error while fetching datas :', error);
+      console.error('Error while fetching datas :', error);
     });
 });
