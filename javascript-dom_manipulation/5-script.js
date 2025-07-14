c2d6e9f47fc2f6d1520a1fec1cf80a3e284d1563
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-14 11:42:29 +0200

    Task 5

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,6 @@
+const updateHeader = document.getElementById('update_header');
+const header = document.querySelector('header');
+
+updateHeader.addEventListener('click', () => {
+  header.textContent = 'New header!!!';
+});
