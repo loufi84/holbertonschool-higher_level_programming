d5803562e665fa05a3e8f1273d278b8f31e94210
Quentin <48597772+loufi84@users.noreply.github.com>
2025-07-23 11:50:27 +0200

    pycode ?

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -2,9 +2,12 @@
 """
 This module will provides a single class
 """
+
+
 class LockedClass:
     """
     This class will prevent user to dynamically create new instance attributes
     excpet if the new instance attribute is called first_name
     """
+
     __slots__ = ['first_name']
